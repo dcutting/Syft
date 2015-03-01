0178c0c4e8fd8b4736951ea17a87ffdbdd268adf
Dan Cutting <dan@cutting.io>
2015-03-01 10:00:47 +0000

    Rename parser to Syft.

---
+++ Syft/Syft.swift
@@ -0,0 +1,182 @@
+public protocol SyftLike {}
+
+public enum Syft: SyftLike {
+
+    case Str(String)
+    case Sequence(SyftLike, SyftLike)
+    case Name(String, SyftLike)
+    case Repeat(SyftLike, minimum: Int, maximum: Int)
+    
+    public func parse(input: String) -> Result {
+        return parse(Remainder(text: input, index: 0))
+    }
+    
+    func parse(input: Remainder) -> Result {
+        switch self {
+            
+        case let .Str(pattern):
+            return parseStr(input, pattern)
+
+        case let .Sequence(first as Syft, second as Syft):
+            return parseSequence(input, [first, second])
+            
+        case let .Name(name, sub as Syft):
+            return parseName(input, name, sub)
+            
+        case let .Repeat(sub as Syft, minimum, maximum):
+            return parseRepeat(input, sub, minimum, maximum, matchesSoFar: 0)
+            
+        default:
+            return .Failure
+        }
+    }
+}
+
+func parseStr(input: Remainder, pattern: String) -> Result {
+    
+    if (pattern.isEmpty || input.text.hasPrefix(pattern)) {
+        
+        let (headText, tailText) = input.text.splitAtIndex(pattern.endIndex)
+        let tailIndex = input.index + distance(headText.startIndex, headText.endIndex)
+        let remainder = Remainder(text: tailText, index: tailIndex)
+        
+        return .Match(match: headText, index: input.index, remainder: remainder)
+    }
+
+    return .Failure
+}
+
+func parseSequence(input: Remainder, subs: [Syft]) -> Result {
+
+    if let head = subs.head {
+        return parseSequence(input, head, subs.tail)
+    } else {
+        return .Match(match: "", index: input.index, remainder: input)
+    }
+}
+
+func parseSequence(input: Remainder, head: Syft, tail: [Syft]) -> Result {
+
+    switch head.parse(input) {
+        
+    case .Failure:
+        return .Failure
+        
+    case let .Match(match: headText, index: headIndex, remainder: headRemainder):
+        let tailResult = parseSequence(headRemainder, tail)
+        return combineSequenceMatch(headText, headIndex, tailResult)
+        
+    case let .Hash(headHash, remainder: headRemainder):
+        let tailResult = parseSequence(headRemainder, tail)
+        return combineSequenceHash(headHash, tailResult)
+        
+    case .Array:
+        return .Failure
+    }
+}
+
+func combineSequenceMatch(headText: String, headIndex: Int, tail: Result) -> Result {
+    
+    switch tail {
+        
+    case .Failure:
+        return .Failure
+        
+    case let .Match(match: tailMatch, index: tailIndex, remainder: tailRemainder):
+        let sequenceRemainder = Remainder(text: tailRemainder.text, index: tailRemainder.index + tailIndex)
+        return .Match(match: headText + tailMatch, index: headIndex, remainder: tailRemainder)
+        
+    case .Hash:
+        return tail
+        
+    case .Array:
+        return .Failure
+    }
+}
+
+func combineSequenceHash(headHash: [String: ResultLike], tail: Result) -> Result {
+
+    switch tail {
+        
+    case .Failure:
+        return .Failure
+        
+    case let .Match(match: _, index: _, remainder: tailRemainder):
+        return .Hash(headHash, remainder: tailRemainder)
+        
+    case let .Hash(tailHash, remainder: tailRemainder):
+        return .Hash(headHash + tailHash, remainder: tailRemainder)
+        
+    case .Array:
+        return .Failure
+    }
+}
+
+func parseName(input: Remainder, name: String, sub: Syft) -> Result {
+
+    let result = sub.parse(input)
+    
+    switch result {
+
+    case .Failure:
+        return .Failure
+
+    case let .Match(match: _, index: _, remainder: remainder):
+        return .Hash([name: result], remainder: remainder)
+        
+    case let .Hash(_, remainder: remainder):
+        return .Hash([name: result], remainder: remainder)
+        
+    case .Array:
+        return .Failure
+    }
+}
+
+func parseRepeat(input: Remainder, sub: Syft, minimum: Int, maximum: Int, #matchesSoFar: Int) -> Result {
+    let shouldAttemptAnotherMatch = matchesSoFar < maximum || maximum < 0
+    
+    if shouldAttemptAnotherMatch {
+        let result = sub.parse(input)
+        switch result {
+            
+        case .Failure:
+            if minimum > 0 && matchesSoFar < minimum {
+                return .Failure
+            } else {
+                return Result.Match(match: "", index: 0, remainder: input)
+            }
+            
+        case let .Match(match: match, index: index, remainder: remainder):
+            let tailResult = parseRepeat(remainder, sub, minimum, maximum, matchesSoFar: matchesSoFar + 1)
+            
+            switch tailResult {
+                
+            case .Failure:
+                return matchesSoFar < minimum ? .Failure : result
+                
+            default:
+                return combineSequenceMatch(match, index, tailResult)
+            }
+            
+        case let .Hash(_, remainder: remainder):
+            let tailResult = parseRepeat(remainder, sub, minimum, maximum, matchesSoFar: matchesSoFar + 1)
+
+            switch tailResult {
+            case let .Match(match: tailMatch, index: tailIndex, remainder: tailRemainder):
+                return Result.Array([result], remainder: tailRemainder)
+            case var .Array(array, remainder: remainder):
+                array.insert(result, atIndex: 0)
+                return Result.Array(array, remainder: remainder)
+            case let .Hash(tailHash, remainder: tailRemainder):
+                return Result.Array([tailResult], remainder: tailRemainder)
+            default:
+                return .Failure
+            }
+            
+        case .Array:
+            return .Failure
+        }
+    } else {
+        return Result.Match(match: "", index: 0, remainder: input)
+    }
+}
--- Syft/main.swift
+++ Syft/main.swift
@@ -1,1 +1,14 @@
 println("Syft")
+
+let a = Syft.Name("anA", Syft.Str("a"))
+let someAs = Syft.Repeat(a, minimum: 0, maximum: 10)
+
+let b = Syft.Name("aB", Syft.Str("b"))
+let someBs = Syft.Repeat(b, minimum: 1, maximum: 10)
+
+//let sequence = Syft.Sequence(Syft.Name("thefirstas", someAs), Syft.Sequence(Syft.Name("thebs", someBs), someAs))
+let sequence = Syft.Sequence(someAs, someBs)
+
+let result = someAs.parse("aa")
+
+println(result)
