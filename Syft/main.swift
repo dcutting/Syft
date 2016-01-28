afe762edceb1506d42280af2ca3a7b9f1299d562
Dan Cutting <dan@cutting.io>
2016-01-28 21:22:12 +0000

    Parse a whole arithmetic expression!

--- Syft/main.swift
+++ Syft/main.swift
@@ -5,15 +5,15 @@ let numeral = Parser.Tag("number", Parser.Repeat(digit, minimum: 1, maximum: nil
 let op = Parser.Either(Parser.Str("+"), Parser.Str("*"))
 let expression = DeferredParser(name: "expression")
 let compound = Parser.Sequence(Parser.Tag("first", numeral), Parser.Sequence(Parser.Tag("op", op), Parser.Tag("second", Parser.Deferred(expression))))
-expression.parser = Parser.Either(numeral, compound)
+expression.parser = Parser.Either(compound, numeral)
 
 let repeatedOnes = Parser.Repeat(Parser.Tag("o", one), minimum: 1, maximum: nil)
 let someOnes = Parser.Tag("ones", repeatedOnes)
 let someTwos = Parser.Repeat(Parser.Tag("t", two), minimum: 1, maximum: nil)
 let someOnesAndTwos = Parser.Sequence(someOnes, someTwos)
 
-let input = "111122"//+1*2"
-let actualResult = someOnesAndTwos.parse(input)
+let input = "12+2*1"
+let actualResult = expression.parse(input)
 
 let expectedResult = Result.Tagged([
     "first": Result.Tagged(["number": Result.Match(match: "12", index: 0)]),
