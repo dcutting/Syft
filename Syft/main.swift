efdb1f285e9e75755cf9177ce0d2049251e7aaac
Dan Cutting <dan@cutting.io>
2016-01-28 23:42:18 +0000

    Example that skips whitespace.

--- Syft/main.swift
+++ Syft/main.swift
@@ -1,11 +1,13 @@
+let space = " \t".any
+let skip = space.some.maybe
 let digit = (0...9).any
-let op = "+-*/".any
-let numeral = digit.some.tag("numeral")
+let op = "+-*/".any.tag("op") >>> skip
+let numeral = skip >>> digit.some.tag("numeral") >>> skip
 let expression = Deferred()
-let compound = numeral.tag("first") >>> op.tag("op") >>> expression.tag("second")
+let compound = numeral.tag("first") >>> op >>> expression.tag("second")
 expression.parser = compound | numeral
 
-let input = "123+52*891/3120"
+let input = "  123+  52     *  891   /3120   "
 let parsed = expression.parse(input)
 
 print(parsed)
