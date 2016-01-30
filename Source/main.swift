dbc93f7ad8d591a109ab88093d04d5d1f9b8d58b
Dan Cutting <dan@cutting.io>
2016-01-30 20:34:08 +0000

    Move Syft to Source and SyftTests to Tests to use SPM defaults.

--- Source/main.swift
+++ Source/main.swift
@@ -1,9 +1,7 @@
-import Regex
-
+// Arithmetic parser.
 let space = " \t\n\r\n".match
 let letter = "az".match
 let skip = space.some.maybe
-//let digit = "0-9a-zA-Z".match
 let digit = (0...9).match
 let op = "+-*/".match.tag("op") >>> skip
 let numeral = skip >>> digit.some.tag("numeral") >>> skip
@@ -11,10 +9,11 @@ let expression = Deferred()
 let compound = numeral.tag("first") >>> op >>> expression.tag("second")
 expression.parser = compound | numeral
 
-//let input = "  123+  52 \t  \n *  891 \r\n  /3120   "
-//let parsed = expression.parse(input)
-//print(parsed)
+let input = "  123+  52 \t  \n *  891 \r\n  /3120   "
+let parsed = expression.parse(input)
+print(parsed)
 
+// Basic regex parser.
 let char = (digit | letter).tag("char")
 let range = char.tag("start") >>> Parser.Str("-") >>> char.tag("end")
 let group = range.tag("range") | char
