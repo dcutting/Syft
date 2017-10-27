ad2e07fbb80ba97849e24e8ee21c664921d5abed
Dan Cutting <dan@cutting.io>
2017-10-27 09:28:45 +0100

    Figuring out the song grammar.

--- Sample/Song.swift
+++ Sample/Song.swift
@@ -17,46 +17,71 @@ struct SongNumber: SongExpression {
 }
 
 func song() -> Pipeline<SongExpression> {
-    return Pipeline(defaultInput: "[].length() = 0\n[_|y].length() = 1.+(y.length())", parser: makeSongParser(), transformer: makeSongTransformer()) { ast in
+//    let input = "[].length() = 0\n[_|y].length() = 1.+(y.length())"
+    let input = ""
+    return Pipeline(defaultInput: input, parser: makeSongParser(), transformer: makeSongTransformer()) { ast in
         let result = ast.evaluate()
         return "\(result)"
     }
 }
 
 func makeSongParser() -> ParserProtocol {
-    let space = " \t".match
-    let skip = space.some.maybe
+    let space = " \t".match.some
+    let skip = space.maybe
+    let pipe = str("|") >>> skip
+    let comma = str(",") >>> skip
+    let lBracket = str("[") >>> skip
+    let rBracket = str("]") >>> skip
+    let lParen = str("(") >>> skip
+    let rParen = str(")")
+
+    let expression = Deferred()
+    let evaluable = Deferred()
+    let pattern = Deferred()
+
+    let listElement = skip >>> evaluable.tag("listElement") >>> skip
+    let listBare = (listElement >>> (comma >>> listElement).some.maybe).maybe.tag("listBare")
+    let listLiteral = (lBracket >>> listBare >>> rBracket).tag("listLiteral")
+
     let newline = str("\n")
     let skipNewlines = newline.some.maybe
     let dot = str(".")
-    let comma = str(",")
-    let leftBracket = str("(")
-    let rightBracket = str(")")
-    let digit = (0...9).match
-    let numeral = digit.some
-    let letter = "abcdefghijklmnopqrstuvwxyz".match
+    let minus = str("-")
     let underscore = str("_")
-    let identifier = (letter | underscore).some.tag("identifier")
-    let functionName = identifier
-    let expression = Deferred()
-    let argument = skip >>> expression >>> skip
-    let arguments = (argument >>> (comma >>> argument).some.maybe).maybe
-    let argumentList = leftBracket >>> arguments >>> rightBracket
-    let literalList = str("[") >>> arguments.maybe >>> str("]")
-    let somethingListPattern = str("[") >>> identifier.tag("head") >>> str("|") >>> identifier.tag("tail") >>> str("]")
-    let listPattern = literalList | somethingListPattern
-    let subject = listPattern | numeral
-    let functionCall = subject >>> dot >>> functionName >>> argumentList >>> skip
-    expression.parser = numeral | functionCall
-    let functionBody = expression
     let assignment = str("=")
-    let pattern = listPattern | numeral
+    let digit = (0...9).match
+    let letter = "abcdefghijklmnopqrstuvwxyz".match
+
+    let numeral = (minus.maybe >>> digit.some).tag("numeral")
+    let identifier = (letter >>> (letter | digit).some.maybe).tag("identifier")
+    let variable = identifier.tag("variable")
+
+    let listTemplate = (lBracket >>> pattern.tag("head") >>> pipe >>> identifier.tag("tail") >>> rBracket).tag("listTemplate")
+    let listPattern = (listLiteral | listTemplate).tag("listPattern")
+
+    let literal = listLiteral | numeral
+
+    let object = (literal | variable).tag("object")
+    let name = identifier.tag("name")
+    let arguments = (lParen >>> listBare >>> rParen).tag("arguments")
+    let call = (object >>> dot >>> name >>> arguments).tag("call")
+
+    evaluable.parser = literal | variable | call
+
+    pattern.parser  = listPattern | listLiteral | numeral | identifier
     let parameter = skip >>> pattern.tag("parameter") >>> skip
-    let parameters = (parameter >>> (comma >>> parameter).some.maybe).maybe
-    let parameterList = leftBracket >>> parameters.tag("parameters") >>> rightBracket
-    let statement = subject.tag("subject") >>> dot >>> functionName.tag("function") >>> parameterList >>> skip >>> assignment >>> skip >>> functionBody.tag("body") >>> skipNewlines
-    let statements = statement.some
-    return statements
+    let parameters = (parameter.recur(1, 1) >>> (comma >>> parameter).some.maybe).tag("parameters")
+    let parameterList = (lParen >>> parameters >>> rParen).maybe.tag("parameterList")
+    let functionBody = evaluable
+
+    let subject = listPattern | literal | variable
+    let rule = subject.tag("subject") >>> dot >>> name.tag("function") >>> parameterList >>> skip >>> assignment >>> skip >>> functionBody.tag("body")
+
+    let rules = rule.recur
+
+    expression.parser = evaluable | rule
+
+    return call
 }
 
 func makeSongTransformer() -> Transformer<SongExpression> {
